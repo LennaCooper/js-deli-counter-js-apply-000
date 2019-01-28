cf33eea1583b7e781207b6498846649da86ba8f1
Lenna Cooper <lenna.cooper@gmail.com>
2019-01-28 16:44:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-function takeANumber(line, personUp){
-  line.push(personUp)
-  return (`Welcome, ${personUp}. You are number ${line.length} in line.`); 
+function takeANumber(line , name ) {
+  line.push(name)
+  return (`Welcome, ${name}. You are number ${line.length} in line.`)
 }
 
-function nowServing(line){
-  if (line.length>0){
-    var person = line.shift();
-    return `Currently serving ${person}.`;
-  } else {
-    return "There is nobody waiting to be served!";
-  }
-}
-
-
-function currentLine(line){
-  var lineString = []
-  if (line.length === 0){
-  return 'The line is currently empty.'
-  } else {
-    for (var i = 0; i < line.length; i++){
-    lineString.push(" " + (i + 1) + `. ${line[i]}`);
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+  var firstPerson = katzDeliLine.shift()
+  return (`Currently serving ${firstPerson}.`)
+    } else {
+      return ("There is nobody waiting to be served!")
     }
-  }
-  return "The line is currently:" + lineString.toString()
 }
 
-
-
-
+function currentLine(line) {
+  if (line.length > 0) {
+    var lineString = []
+    for (let i = 0; i < line.length; i++) {
+      lineString.push(" " + (i + 1) + `. ${line[i]}`) 
+    }
+  } else {
+    return ("The line is currently empty.")
+  }
+  return 'The line is currently:' + lineString.toString()
+}
   
